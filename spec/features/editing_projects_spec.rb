40c3dfc6e4318b979003fda61d1e5603391855ad
Leyaim <leyaim.jimenez@michelada.io>
2018-11-16 18:34:47 -0600

    editing finished

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -1,20 +1,6 @@
 require "rails_helper"
 
 RSpec.feature "Users can edit existing projects" do
-<<<<<<< HEAD
-  scenario "with valid attributes" do
-    FactoryGirl.create(:project, name: "Sublime Text 3")
-
-    visit "/"
-    click_link "Sublime Text 3"
-    click_link "Edit Project"
-    fill_in "Name", with: "Sublime Text 4 beta"
-    click_button "Update Project"
-
-    expect(page).to have_content "Project has been updated."
-    expect(page).to have_content "Sublime Text 4 beta"
-  end
-=======
 
     before do
         FactoryGirl.create(:project, name: "Sublime Text 3")
@@ -39,5 +25,4 @@ RSpec.feature "Users can edit existing projects" do
         expect(page).to have_content "Project has not been updated."
       end
 
->>>>>>> b200ba18f035c211fcd94c18bc66286ce8d0bf50
 end
